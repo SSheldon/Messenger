24a39645589dbd2184a1c0d572a922bcc02caf8f
Steven Sheldon <steven.sheldon@live.com>
2011-11-21 20:59:38 -0600

    Began implementing server.

--- MessengerServer/Server.cs
+++ MessengerServer/Server.cs
@@ -1,11 +1,43 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace Messenger
 {
     public class Server
     {
+        Socket sock;
+
+        public Server(int port = 4560)
+        {
+            sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sock.Bind(new IPEndPoint(IPAddress.Any, port));
+            sock.Listen(10);
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                HandleCientConnection();
+            }
+        }
+
+        public void HandleCientConnection()
+        {
+            Socket client = sock.Accept();
+            byte[] buffer = new byte[128];
+            int bytesRead = client.Receive(buffer);
+            Console.WriteLine(ASCIIEncoding.ASCII.GetString(buffer, 0, bytesRead));
+            client.Shutdown(SocketShutdown.Both);
+            client.Close();
+        }
+
         public static void Main(string[] args)
         {
+            Server svr = new Server();
+            svr.Run();
         }
     }
 }
